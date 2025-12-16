f0b70221bef1920beed0f6a88dc4ee81039ffe67
killfly <realkillfly@gmail.com>
2025-12-16 12:23:39 -0600

    Add My Invites page

---
+++ Data/StoryInvite.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace mass.Data
+{
+    public class StoryInvite
+    {
+        public int Id { get; set; }
+
+        public DateTimeOffset CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTimeOffset? AcceptedAt { get; set; }
+        public bool IsAccepted => AcceptedAt.HasValue;
+
+        public required virtual Story Story { get; set; }
+        public required virtual MassIdentityUser InvitedUser { get; set; }
+        public required virtual MassIdentityUser InvitedBy { get; set; }
+    }
+}
