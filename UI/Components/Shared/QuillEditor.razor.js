30137be2a8186f446ea8cbea815917e5f7ac4544
killfly <realkillfly@gmail.com>
2026-01-19 09:41:05 -0600

    Almost working

---
+++ UI/Components/Shared/QuillEditor.razor.js
@@ -0,0 +1,49 @@
+let quillInstances = {};
+
+export function initializeQuill(editorId, initialContent, placeholder) {
+    const container = document.getElementById(editorId);
+    
+    if (!container) {
+        console.error(`Container with id ${editorId} not found`);
+        return null;
+    }
+
+    const quill = new Quill(`#${editorId}`, {
+        theme: 'snow',
+        placeholder: placeholder,
+        modules: {
+            toolbar: [
+                ['bold', 'italic', 'underline', 'strike'],
+                ['blockquote', 'code-block'],
+                [{ 'list': 'ordered'}, { 'list': 'bullet' }],
+                [{ 'header': 1 }, { 'header': 2 }],
+                ['clean']
+            ]
+        }
+    });
+
+    if (initialContent) {
+        quill.root.innerHTML = initialContent;
+    }
+
+    quillInstances[editorId] = quill;
+}
+
+export function getQuillContent(editorId) {
+    const quill = quillInstances[editorId];
+    if (quill) {
+        return quill.root.innerHTML;
+    }
+    return '';
+}
+
+export function setQuillContent(editorId, content) {
+    const quill = quillInstances[editorId];
+    if (quill) {
+        quill.root.innerHTML = content;
+    }
+}
+
+export function disposeQuill(editorId) {
+    delete quillInstances[editorId];
+}
